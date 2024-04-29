ecfd1e9c406507748f54316ef78c92fd776a5f57
Saenro Gonsalves <aenros0902@gmail.com>
2024-04-29 12:30:51 +0530

    !st push commit

---
+++ Casetools_SE_set1_225/src/pcat/CAT.java
@@ -0,0 +1,20 @@
+package pcat;
+
+public class CAT {
+	private String name;
+	private String colour;
+	public CAT(String name,String breed) {
+		this.name=name;
+		this.colour=colour;
+	}
+	public String getColour() {
+		return colour;
+	}
+	//Saenro Gonsalves <aenros0902@gmail.com>
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
